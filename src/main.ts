296381f5b0db465a44160f5a7b2074588e84ff60
Elvijs <elvijsostrovskis@gmail.com>
2023-11-02 20:36:25 -0500

    Make changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { fromEvent, map } from 'rxjs';
 
-import { AppModule } from './app/app.module';
+// https://gist.github.com/tuan/8a1ca1b0376a5056f4141972b171c89e
 
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div>a</div>
+    <div *ngIf="url$ | async as url">{{ url }}</div>
+    <div>c</div>
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    <button (click)="handleClick()">Post Message</button>
+  `,
+})
+export class App {
+  url$ = fromEvent<MessageEvent>(window, 'message').pipe(
+    map(({ data }) => data)
+  );
+
+  handleClick() {
+    postMessage('https://jsonplaceholder.typicode.com/todos/1', '*');
+  }
+}
+
+bootstrapApplication(App);
